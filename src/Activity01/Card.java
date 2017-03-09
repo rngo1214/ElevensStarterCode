88e642c651441da44149e2c105163799754c8d72
2006218 <2006218@AL-105-18.student.acalanes.k12.ca.us>
2017-03-09 12:09:12 -0800

    Minor edits

--- src/Activity01/Card.java
+++ src/Activity01/Card.java
@@ -97,6 +97,6 @@ public class Card {
 	//@Override
 	public String toString() {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-                return this.rank + " of " + this.suit + "(point value = " + this.pointValue +")";
+                return this.rank + " of " + this.suit + " (point value = " + this.pointValue +")";
 	}
 }
